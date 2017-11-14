825f44bcaccecace813550af35a86c90a3be1d9e
Jonathan Constant <johnny45gotu@outlook.com>
2017-11-14 11:08:12 -0500

    Added improved permission to use profile feed items API

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
 
 from . import serializers
 from . import models
@@ -83,7 +86,7 @@ class HelloViewSet(viewsets.ViewSet):
 
 
         if serializer.is_valid():
-            name = serializer.data.get('name')
+            name    = serializer.data.get('name')
             message = 'Hello {0}'.format(name)
             return Response({'message': message})
         else:
@@ -115,12 +118,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, creating and updating profiles."""
 
 
-    serializer_class = serializers.UserProfileSerializer
-    queryset = models.UserProfile.objects.all()
+    serializer_class       = serializers.UserProfileSerializer
+    queryset               = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes =(permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name','email',)
+    permission_classes     =(permissions.UpdateOwnProfile,)
+    filter_backends        = (filters.SearchFilter,)
+    search_fields          = ('name','email',)
+
 
 class LoginViewSet(viewsets.ViewSet):
     """Checks email and password and returns an auth token."""
@@ -132,3 +136,19 @@ class LoginViewSet(viewsets.ViewSet):
 
         return ObtainAuthToken().post(request)
 
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class       = serializers.ProfileFeedItemSerializer
+    queryset               =  models.ProfileFeedItem.objects.all()
+    permission_classes     = (permissions.PostOwnStatus, IsAuthenticated) 
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)
+
+
+         
